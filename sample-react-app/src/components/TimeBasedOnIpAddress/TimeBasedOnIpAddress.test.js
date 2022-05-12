1e0e1d4860b0703d46219654daee336ddedab20c
Burak Geylani <burak.geylani@ithinka.com>
2022-05-12 11:37:46 +0300

    test for bad build

--- sample-react-app/src/components/TimeBasedOnIpAddress/TimeBasedOnIpAddress.test.js
+++
@@ -1,8 +0,0 @@
-import {render} from '@testing-library/react';
-import TimeBasedOnIpAddress from './TimeBasedOnIpAddress';
-
-test('renders a section of the page with "Loading..." text when the page is first mounted', () => {
-    const {getByTestId} = render(<TimeBasedOnIpAddress />);
-    const headerElement = getByTestId('time-ip-address');
-    expect(headerElement.textContent).toBe('Loading...')
-});
